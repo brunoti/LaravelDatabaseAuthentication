7a56040adf1ddf62fdce37bb8e510dcf2c66f87a
Bruno Oliveira <bruno.oliveirati1@gmail.com>
2014-08-28 17:05:54 -0300

    Complete 1.0

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -4,9 +4,28 @@
 
 <div class="container">
 	<div class="page-header">
-		<h1>Bem-vindo!</h1>		
+		<h1>Bem-vindo, {{$usuario}}!</h1>		
 	</div>
-	
+	<div class="row">
+		<div class="col-lg-12">
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($usuarioLista as $user)
+					<tr>
+						<th>{{$user->username}}</th>
+						<th>{{$user->email}}</th>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>		
+	</div>
 </div>
 
 @stop
